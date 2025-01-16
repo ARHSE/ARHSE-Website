a6061b2ef044ad043ef823bede83a26960318499
ARHSE <adrian-ramon-held@outlook.com>
2025-01-16 10:15:27 +0100

    Update about.js

--- data/about.js
+++ data/about.js
@@ -5,14 +5,14 @@ const aboutContent = {
     sectionText: "About Me",
     navbarText: "About",
     content: `Level B (CSEP) certified systems engineer with experience in the aerospace, automotive and defence industry.
-I am passionate about solving challenging engineering problems, coaching others and being a bridge between the customers and the developers.
+              I am passionate about solving challenging engineering problems, coaching others and being a bridge between the customers and the developers.
 `,
   },
   de: {
     sectionText: "Über mich",
     navbarText: "Über",
     content: `Zertifizierter Systemingenieur der Stufe B (CSEP) mit Erfahrung in der Luft- und Raumfahrt-, Automobil- und Rüstungsindustrie.
-Meine Leidenschaft ist es, anspruchsvolle technische Probleme zu lösen, andere zu coachen und eine Brücke zwischen Kunden und Entwicklern zu sein.
+              Meine Leidenschaft ist es, anspruchsvolle technische Probleme zu lösen, andere zu coachen und eine Brücke zwischen Kunden und Entwicklern zu sein.
 `,
   },
 };
